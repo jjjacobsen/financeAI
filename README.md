e9ffe3c80b421bb95f3fe54edc69dfebfcee37ee
Jonah Jacobsen <jjacobsen115@gmail.com>
2024-10-04 16:08:40 -0500

    update README and set main to work with new class imports and pre-commit

--- README.md
+++ README.md
@@ -5,80 +5,62 @@ Using AI to calculate a budget and give financial advice based on my current fin
     - Use AI to do the sorting
 - Can access my transaction history through apis, and make sure not to let any kind of passwords end up on the public repo
 - Can also access my loan information and other assets through apis
-- Have this code put everything into the Google Sheet so I can see it easily
-- Once I have all this data, use AI to calculate a budget and give financial advice
+- Have code put everything into the Google Sheet so I can see it easily
+    - maybe eventually I move data to a database and make a fancy UI. For now, Google Sheets is good enough
+- Once I collect all the data, use AI to calculate a budget and give financial advice
 
 
-### Notes ###
-- Seems like Gorgonia is the AI library for Go
-    - https://gorgonia.org/
-- I really would like this to do three things (these are the exported functions)
-    - pull transactions via api or web scraping
-    - group the transactions
-    - put the sum of the groups into my net worth Google doc
-        - if it does this then calculating the net worth stuff becomes very easy
-- These are bonus things that would be nice to have
-    - get all other financial data via api
-        - loans
-        - investments
-    - given the data above, to give financial advice based on those numbers
-- Build an executable and put it in my PATH so that I can use this code easily from the cli
-    - given the project structure below, use this command
-    - `go build -o /usr/local/bin/yourapp ./cmd/financeAI`
-    - not sure if this does the permissions correctly, we will have to see
-- After investigation I decided to use a Random Forest ML model
-    - after conversing with the AI this seems to be my best choice
-    - was originally going to use the Gorgonia library, however that doesn't support random forest so I'm using GoLearn instead
-        - https://github.com/sjwhitworth/golearn
-- When I asked the AI for how I should organize the repo, it gave me this. I like it
-```
-/
-├── cmd
-│   └── yourapp
-│       └── main.go  # Contains the main function and CLI setup.
-├── pkg
-│   ├── processor
-│   │   └── processor.go  # Business logic for processing data.
-│   ├── loader
-│   │   └── loader.go  # Logic for loading/saving data.
-│   └── model
-│       └── model.go  # Definitions of data models and possibly machine learning models.
-├── data
-│   └── ...           # Data files, possibly ignored in .gitignore for large datasets.
-├── scripts
-│   └── setup.sh      # Scripts for setting up or configuring the environment.
-├── go.mod
-├── go.sum
-└── README.md
-```
-- there is an example file under cmd for how to build a random forest model using GoLearn. Take that and build all these pkg's out
-- I can save the output of a trained model using encoding/gob, just ask the AI how to do it
-- to build these training datasets I might need to make a custom tool that I can click in a gui to speed sort all the transactions
-    - like I collect all the transactions then display them one at a time in the gui, tinder style, and select which group it belongs
-    - as I go through and manually sort, it builds the training dataset on each click
-- since I have a limited number of factors, trying different parameters for the model is how I will find the highest accuracy
-    - like building 150 trees instead of 100, etc
-- I want to make sure that wherever I run this binary from, the data and models are always stored in the data directory in this repo
-    - might need to create and use scripts/setup.sh for that
+### Setup ###
 
-### Python Packages ###
+1. Install pyenv and pyenv-virtualenv ([brew](https://brew.sh/))
+   ```sh
+   brew update
+   brew upgrade
+   brew install pyenv
+   brew install pyenv-virtualenv
+   ```
+2. Install python and create the venv
+   ```sh
+   pyenv install <python version>
+   pyenv virtualenv <python version> <venv name>
+   ```
+    - for automatic sourcing of the venv
+        ```sh
+        pyenv local <venv name>
+        ```
+    - Current version is 3.12.6
+3. Install pdm
+    ```sh
+    pip install pdm
+    ```
+    - Current version is 2.19.1
+4. Install the rest of the packages
+    ```sh
+    pdm install --dev
+    ```
+5. Setup pre-commit
+    ```sh
+    pre-commit install
+    ```
 
-For now I'm still using requirements.txt. I want to learn something new like pdm or poetry and do this in a better way. Here are the high level packages I installed so I can remember before I learn the new stuff
-
-- gspread
-- google-auth
-- beautifulsoup4
-- pandas
-- ipython
 
 ### To setup manual.py ###
 
 Needed to do a few things
 - create new google cloud project
-- enable services
-    - sheets.googleapis.com
-    - drive.googleapis.com
-- create service account and download json key
-- from the sheet, share it with the service account email
-- login to bank and expand the transactions history to what you need, then download the html
+    - enable services
+        - sheets.googleapis.com
+        - drive.googleapis.com
+    - create service account and download json key
+- from the sheet ui, share it with the service account email
+- for restricted files that need a symbolic link:
+    - `ln -s ~/path/to/file restricted/file`
+
+
+### To run ###
+
+- login to bank and expand the transactions history to get needed transactions, then download the html
     - right click -> save page as -> Format: Web Page, Complete -> ledger.html
+        - this also downloads some directory with extra files, but ledger.html is the only one needed
+    - ledger.html needs to be under restricted/
+- `python main.py`
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from restricted import GoogleSheetUpdater, WebScraper
+# flake8: noqa: E501
+from restricted.google import GoogleSheetUpdater
+from restricted.scrape import WebScraper
 
 
 def main():
@@ -14,72 +16,93 @@ def main():
         print("3. Update Data with AI")
         choice = input("Enter your choice: ")
 
-        if choice == '1':
+        if choice == "1":
             print("\nNot implemented yet")
-        elif choice == '2':
+        elif choice == "2":
             month = input("\nWhich month do you want to update? (e.g. 11/2023): ")
-            if month == 'q':
+            if month == "q":
                 continue
 
             gsUpdater.set_worksheet(month)
             df = webScraper.filter_by_month(month)
             total_transactions_count = len(df)
-            end_of_month_balance = round(float(df.iloc[0]['Account Balance'][1:].replace(",", "")), 2)
+            end_of_month_balance = round(
+                float(df.iloc[0]["Account Balance"][1:].replace(",", "")), 2
+            )
             categories = {
-                '1': ['rent + utilities', 0],
-                '2': ['other monthly payments', 0],
-                '3': ['groceries', 0],
-                '4': ['food / whiskey / cigars', 0],
-                '5': ['gas / parking / transportation', 0],
-                '6': ['fun', 0],
-                '7': ['insurance', 0],
-                '8': ['vehicle payments', 0],
-                '9': ['student loans payments', 0],
-                '10': ['misc', 0],
-                '11': ['earned', 0]
+                "1": ["rent + utilities", 0],
+                "2": ["other monthly payments", 0],
+                "3": ["groceries", 0],
+                "4": ["food / whiskey / cigars", 0],
+                "5": ["gas / parking / transportation", 0],
+                "6": ["fun", 0],
+                "7": ["insurance", 0],
+                "8": ["vehicle payments", 0],
+                "9": ["student loans payments", 0],
+                "10": ["misc", 0],
+                "11": ["earned", 0],
             }
             index = 0
             print(f"Set worksheet to {month}")
 
             while index < total_transactions_count:
-                print(f"\nTransaction {index}/{total_transactions_count} ({index / total_transactions_count * 100:.2f}%)")
+                print(
+                    f"\nTransaction {index}/{total_transactions_count} ({index / total_transactions_count * 100:.2f}%)"
+                )
                 print(f"{df.iloc[index]}")
-                sub_choice = input("\t".join([f"{key}) {value[0]}" for key, value in categories.items()]) + "\tprint) show current values\n")
-                if sub_choice == 'q':
+                sub_choice = input(
+                    "\t".join(
+                        [f"{key}) {value[0]}" for key, value in categories.items()]
+                    )
+                    + "\tprint) show current values\n"
+                )
+                if sub_choice == "q":
                     break
-                elif sub_choice == 'print':
+                elif sub_choice == "print":
                     print(categories)
                     continue
                 elif sub_choice in categories:
-                    str_value = df.iloc[index]['Transaction Price']
-                    if str_value[0] == '$':
+                    str_value = df.iloc[index]["Transaction Price"]
+                    if str_value[0] == "$":
                         float_value = float(str_value[1:].replace(",", ""))
                     else:
                         float_value = float(str_value[2:].replace(",", ""))
-                    categories[sub_choice][1] = round(categories[sub_choice][1] + float_value, 2)
+                    categories[sub_choice][1] = round(
+                        categories[sub_choice][1] + float_value, 2
+                    )
                     index += 1
                 else:
                     print("Invalid choice. Please try again")
                     continue
 
-            if sub_choice != 'q':
-                print(f"\nTransaction {index}/{total_transactions_count} ({index / total_transactions_count * 100:.2f}%)")
+            if sub_choice != "q":
+                print(
+                    f"\nTransaction {index}/{total_transactions_count} ({index / total_transactions_count * 100:.2f}%)"
+                )
                 print("Attempting to push data to Google Sheets...")
 
-                total_expenses = sum(value[1] for key, value in categories.items() if key != '11')
+                total_expenses = sum(
+                    value[1] for key, value in categories.items() if key != "11"
+                )
 
-                values = [[name, round(amount, 2)] for name, amount in categories.values()]
+                values = [
+                    [name, round(amount, 2)] for name, amount in categories.values()
+                ]
                 values.append(["total expenses", round(total_expenses, 2)])
-                values.append(["net monthly", round(categories['11'][1] - total_expenses, 2)])
+                values.append(
+                    ["net monthly", round(categories["11"][1] - total_expenses, 2)]
+                )
 
                 gsUpdater.update(values, "A2")
-                gsUpdater.update([["total number of transactions", total_transactions_count]], "A16")
+                gsUpdater.update(
+                    [["total number of transactions", total_transactions_count]], "A16"
+                )
                 gsUpdater.update([["checking", end_of_month_balance]], "A19")
                 print("Pushed data to Google Sheets")
 
-        elif choice == '3':
+        elif choice == "3":
             print("\nNot implemented yet")
-        elif choice == 'q':
+        elif choice == "q":
             print("\nExiting...")
             break
         else:
